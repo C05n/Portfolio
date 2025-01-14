c56c44817ccaf80446700be0c02309d9832c7bc2
C05n <Erh.cedric@gmail.com>
2025-01-14 21:11:23 +0100

    Responsive page contact

--- src/containers/contactCards/contactCards.jsx
+++ src/containers/contactCards/contactCards.jsx
@@ -3,7 +3,7 @@ import ContactCard from "../../components/contactCard/contactCard"
 function ContactCards () {
    return (
       <div className="contact_cards">
-         <ContactCard $cardlink="src\assets\svg\mail.svg" $cardtext="Envoyez moi un E-mail" />
+         <ContactCard $cardlink="src\assets\svg\tools\github.svg" $cardtext="Retrouvez moi sur GitHub" />
          <ContactCard $cardlink="src\assets\svg\linkedin.svg" $cardtext="Contactez moi sur LinkedIn" />
       </div>
    )
