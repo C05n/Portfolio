f77dafea5596e4a4e13a6992dff0eafcf62a8e56
C05n <Erh.cedric@gmail.com>
2025-01-15 19:31:00 +0100

    Mise à jour project_card

--- src/components/projectsCard/projectsCard.jsx
+++ src/components/projectsCard/projectsCard.jsx
@@ -1,21 +1,20 @@
-function ProjectsCard({ project, activeProject, onProjectClick }) {
-    return (
-       <div 
-          className={`project_card ${activeProject && activeProject.id === project.id ? 'active' : ''}`} 
-          onClick={() => onProjectClick(project)}
-       >
-          <img className="project_card-img" src={project.cover} alt={project.title} />
-          <h2 className="project_card-title">{project.title}</h2>
-          <p className="project_card-subtitle">{project.subtitle}</p>
-          <p className="project_card-desc">{project.description}</p>
-          <a className="project_card-link" href="">En savoir plus...</a>
-          <div className="project_card-stacks">
-             {project.stacks.map((stack) => (
+import { NavLink } from "react-router-dom";
+
+function ProjectsCard({project}) {
+   return (
+      <div className="project_card">
+         <img className="project_card-img" src={project.cover} alt={project.title} />
+         <h2 className="project_card-title">{project.title}</h2>
+         <p className="project_card-subtitle">{project.subtitle}</p>
+         <p className="project_card-desc">{project.description}</p>
+         <NavLink to={project.link} className="project_card-link" target="_blank">En savoir plus...</NavLink>
+         <div className="project_card-stacks">
+            {project.stacks.map((stack) => (
                <p className="stack-icon">{stack}</p>
-             ))}
-          </div>
-       </div>
-    );
+            ))}
+         </div>
+      </div>
+   );
  }
  
  export default ProjectsCard;
