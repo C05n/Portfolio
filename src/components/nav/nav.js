cc15e6e741a1cfda9dc02974e0c329c832eee685
C05n <159690716+C05n@users.noreply.github.com>
2025-10-18 07:55:10 +0200

    Refactor Nav component to simplify visibility handling

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,21 +1,15 @@
 "use client";
 import React, { useState } from 'react';
 import style from "./nav.module.css";
-import { IoCodeSlash, IoMail } from "react-icons/io5";
+import { IoCodeSlash } from "react-icons/io5";
 
 export default function Nav({ onToggleVisibility }) {
    const [isProjetsVisible, setIsProjetsVisible] = useState(false);
-   const [isProfilesVisible, setIsProfilesVisible] = useState(false);
 
    const handleToggleProjets = () => {
       setIsProjetsVisible(prev => !prev);
       onToggleVisibility('projets', !isProjetsVisible);
    };
-      const handleToggleProfiles = () => {
-      setIsProfilesVisible(prev => !prev);
-      onToggleVisibility('profiles', !isProfilesVisible);
-   };
-
    return (
       <nav className={style.nav}>
          <div className={style.nav_button_container}>
@@ -24,12 +18,7 @@ export default function Nav({ onToggleVisibility }) {
             </button>
             <p className={style.nav_button_container_text}>Projets</p>
          </div>
-         <div className={style.nav_button_container}>
-            <button onClick={handleToggleProfiles} className={style.nav_button_container_btn}>
-               <IoMail size={30} />
-            </button> 
-            <p className={style.nav_button_container_text}>Réseaux</p>
-         </div>
       </nav>
    )
+
 }
